2b724787fd43c137b7951ae3b1b95b4963cb8e94
Stepan Paholyk <stepanpaholyk@gmail.com>
2016-11-12 14:41:23 +0200

    Initial Commit

---
+++ Ligue/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Ligue
+//
+//  Created by Stepan Paholyk on 11/12/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Ligue/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Ligue
+//
+//  Created by Stepan Paholyk on 11/12/16.
+//  Copyright © 2016 Stepan Paholyk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
